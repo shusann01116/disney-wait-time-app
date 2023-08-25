b4e904df730d97c88714b744284b2912089383a4
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2023-08-26 01:45:28 +0900

    feat: add an Event Bridge rule invokes the lambda every 5 min

--- terraform/modules/app/collector/lambda.tf
+++ terraform/modules/app/collector/lambda.tf
@@ -19,7 +19,7 @@ module "collector" {
   allowed_triggers = {
     CronRule = {
       principal  = "events.amazonaws.com"
-      source_arn = aws_cloudwatch_event_target.invoke_collector.arn
+      source_arn = aws_cloudwatch_event_rule.cron_rule.arn
     }
   }
 
@@ -37,3 +37,14 @@ module "collector" {
   }
 }
 
+resource "aws_cloudwatch_event_rule" "cron_rule" {
+  name        = "disney-waittime-app-collector-scheduler-${var.env}"
+  description = "Schedule to run the collector lambda function"
+
+  schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "invoke_collector" {
+  rule = aws_cloudwatch_event_rule.cron_rule.name
+  arn  = module.collector.lambda_function_arn
+}
