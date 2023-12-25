c13dfd4e330e2c59a9fcedd12ddcedc77d8f402c
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2023-12-25 21:46:37 +0900

    Remove NextUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,10 @@
 import type { Config } from "tailwindcss";
-import { nextui } from "@nextui-org/react";
 
 const config: Config = {
-  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -18,6 +15,5 @@ const config: Config = {
       },
     },
   },
-  plugins: [nextui()],
 };
 export default config;
