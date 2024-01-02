c80cc976a55064a9262f1925acbc7d7723b74e61
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2024-01-02 23:13:33 +0900

    Support greetins

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,54 +1,105 @@
 "use server";
 
-type FacilityOperatingHours = {
-  OperatingHoursFromDate: string;
-  OperatingHoursFrom: string;
-  OperatingHoursToDate: string;
-  OperatingHoursTo: string;
-  SunsetFlg: boolean;
-  OperatingStatusCD: string | null;
-  OperatingStatus: string | null;
-  OperatingChgFlg: boolean;
+import { Facility, getLink } from "@/lib/types";
+
+export type FacilityResp = {
+  id: string;
+  name: string;
+  operatingStatus: { id: string; name: string };
+  operatingHour: { from: Date; to: Date };
+  standbyTime: number;
+  updatedAt: Date;
 };
 
-type Facility = {
-  FacilityID: string;
-  FacilityName: string;
-  FacilityKanaName: string;
-  NewFlg: boolean;
-  FacilityURLSP: string | null;
-  FacilityStatusCD: string | null;
-  FacilityStatus: string | null;
-  StandbyTime: boolean | string | null;
-  OperatingHoursFromDate: string;
-  OperatingHoursFrom: string;
-  OperatingHoursToDate: string;
-  OperatingHoursTo: string;
-  OperatingStatusCD: string;
-  OperatingStatus: string;
-  SunsetFlg: boolean;
-  Fsflg: boolean;
-  FsStatusflg: string | null;
-  FsStatus: string | null;
-  FsStatusCD: string | null;
-  FsStatusStartDate: string | null;
-  FsStatusStartTime: string | null;
-  FsStatusEndDate: string | null;
-  FsStatusEndTime: string | null;
-  UseLimitFlg: boolean;
-  UseStandbyTimeStyle: boolean;
-  OperatingChgFlg: boolean;
-  UpdateTime: string;
-  operatingHours: FacilityOperatingHours[];
-};
-
-export async function GetFacilities(park: string) {
-  var rawData = await getData(links[park]);
-  if (rawData == null) {
-    return [{ FacilityName: "failed", FacilityID: "0" } as Facility];
+export async function getStandbys(park: string): Promise<FacilityResp[]> {
+  const attractions = await getAttractions(park);
+  if (attractions === null) {
+    return [];
   }
 
-  return JSON.parse(rawData) as Facility[];
+  const greetings = await getGreetings(park);
+  if (greetings === null) {
+    return [];
+  }
+
+  return [...attractions, ...greetings];
+}
+
+export async function getAttractions(park: string): Promise<FacilityResp[]> {
+  const link = getLink(park);
+  if (link == null) {
+    return [];
+  }
+
+  const attractions = await getData(link?.attraction);
+  if (attractions == null) {
+    return [];
+  }
+
+  const facilities = JSON.parse(attractions) as Facility[];
+
+  return facilities.map((f) => ({
+    id: f.FacilityID,
+    name: f.FacilityName,
+    operatingStatus: {
+      id: f.OperatingStatusCD ?? "",
+      name: f.OperatingStatus ?? "",
+    },
+    operatingHour: {
+      from: new Date(`${f.OperatingHoursFromDate} ${f.OperatingHoursFrom}:00`),
+      to: new Date(`${f.OperatingHoursToDate} ${f.OperatingHoursTo}:00`),
+    },
+    standbyTime:
+      typeof f.StandbyTime === "string" ? parseInt(f.StandbyTime, 10) : 0,
+    updatedAt: new Date(f.UpdateTime),
+  }));
+}
+
+export async function getGreetings(park: string): Promise<FacilityResp[]> {
+  const link = getLink(park);
+  if (link == null) {
+    return [];
+  }
+
+  const greetings = await getData(link.greeting);
+  if (greetings == null) {
+    return [];
+  }
+
+  const parsedData = JSON.parse(greetings);
+  const facilities: FacilityResp[] = [];
+
+  Object.values(parsedData).forEach((area: any) => {
+    area.Facility.forEach((facilityWrapper: any) => {
+      const greeting = facilityWrapper.greeting;
+      if (greeting) {
+        const operatingHours = greeting.operatinghours?.[0] || {};
+        facilities.push({
+          id: greeting.FacilityID,
+          name: greeting.FacilityName,
+          operatingStatus: {
+            id: operatingHours.OperatingStatusCD ?? "",
+            name: operatingHours.OperatingStatus ?? "",
+          },
+          operatingHour: {
+            from: new Date(
+              `${operatingHours.OperatingHoursFromDate} ${operatingHours.OperatingHoursFrom}:00`,
+            ),
+            to: new Date(
+              `${operatingHours.OperatingHoursToDate} ${operatingHours.OperatingHoursTo}:00`,
+            ),
+          },
+          standbyTime:
+            typeof greeting.StandbyTime === "string"
+              ? parseInt(greeting.StandbyTime, 10)
+              : 0,
+          updatedAt: new Date(greeting.UpdateTime),
+        });
+      }
+    });
+  });
+
+  return facilities;
 }
 
 async function getData(url: string) {
@@ -67,12 +118,3 @@ async function getData(url: string) {
 
   return res.text();
 }
-
-interface Dictionary<T> {
-  [Key: string]: T;
-}
-
-const links: Dictionary<string> = {
-  tdl: "https://www.tokyodisneyresort.jp/_/realtime/tdl_attraction.json",
-  tds: "https://www.tokyodisneyresort.jp/_/realtime/tds_attraction.json",
-};
